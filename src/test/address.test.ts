f17dfe0f407fa77b12ab8529a832224028425a15
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2025-01-04 10:15:41 +0800

    feat: remove address api

--- src/test/address.test.ts
+++ src/test/address.test.ts
@@ -271,3 +271,38 @@ describe("DELETE /api/contacts/:contactId/addresses/:addressId", () => {
     expect(response.body.errors).toBeDefined();
   });
 });
+
+describe("GET /api/contacts/:contactId/addresses", () => {
+  beforeEach(async () => {
+    await UserTest.create();
+    await ContactTest.create();
+    await AddressTest.create();
+  });
+
+  afterEach(async () => {
+    await AddressTest.deleteAll();
+    await ContactTest.deleteAll();
+    await UserTest.delete();
+  });
+
+  it("should be able to list address", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .get(`/api/contacts/${contact.id}/addresses`)
+      .set("X-API-TOKEN", "test");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+  });
+
+  it("should reject list address if contact is not found", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .get(`/api/contacts/${contact.id + 1}/addresses`)
+      .set("X-API-TOKEN", "test");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(404);
+    expect(response.body.errors).toBeDefined();
+  });
+});
